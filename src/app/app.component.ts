b0d175c5d3cd1d56a4c8a5a5da84fd3d2f86aa68
stsogoo <stsogoo@vmware.com>
2019-11-25 14:27:15 -0800

    fix: show demo of wrong view insertion place
    Signed-off-by: stsogoo <stsogoo@vmware.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Directive, Optional, SkipSelf, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({ selector: '[previouslySibling]', host: {
+  '[class.previously-sibling]':'true'
+} })
+export class PreviouslySibling {
+  constructor(
+    private template: TemplateRef<any>,
+    @Optional()
+    @SkipSelf()
+    private vc: ViewContainerRef){
+      vc.createEmbeddedView(template);
+    }
+}
+
+@Directive({ selector: '[parentViewContainer]', host: {
+  '[class.parent-view-container]':'true'
+} })
+export class ParentViewContainer {
+  constructor(private vcr: ViewContainerRef) {}
+}
 
 @Component({
   selector: 'app-root',
